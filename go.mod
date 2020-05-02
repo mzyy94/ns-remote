bb03023a9be8f2acf41be6d0a67eff94078faf5c
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-05-02 21:52:18 +0900

    Change output to webrtc

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ns-remote
 
 go 1.14
 
-require github.com/notedit/gst v0.0.4
+require (
+	github.com/notedit/gst v0.0.4
+	github.com/pion/webrtc/v2 v2.2.7
+)
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"os"
 
 	"github.com/notedit/gst"
+	"github.com/pion/webrtc/v2"
+	"github.com/pion/webrtc/v2/pkg/media"
 )
 
 func main() {
@@ -29,22 +36,81 @@ func main() {
 	h264VideoCap := gst.CapsFromString("video/x-h264,width=1280,height=720,stream-format=byte-stream")
 	convertFilter.SetObject("caps", h264VideoCap)
 
-	parser, _ := gst.ElementFactoryMake("h264parse", "parser")
-
-	rtph264pay, _ := gst.ElementFactoryMake("rtph264pay", "rtph264pay")
-	rtph264pay.SetObject("config-interval", -1)
-	rtph264pay.SetObject("pt", 96)
-
 	sink, _ := gst.ElementFactoryMake("appsink", "sink")
 
-	pipeline.AddMany(source, filter, convert, convertFilter, parser, rtph264pay, sink)
+	pipeline.AddMany(source, filter, convert, convertFilter, sink)
 
 	source.Link(filter)
 	filter.Link(convert)
 	convert.Link(convertFilter)
-	convertFilter.Link(parser)
-	parser.Link(rtph264pay)
-	rtph264pay.Link(sink)
+	convertFilter.Link(sink)
+
+	// WebRTC setup
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+
+	// Create a video track
+	videoTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeH264, rand.Uint32(), "video", "video")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("waiting for signal")
+	// Wait for the offer to be pasted
+	offer := webrtc.SessionDescription{}
+
+	in, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(b, &offer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(offer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create an answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output the answer in base64 so we can paste it in browser
+	b, err = json.Marshal(answer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(base64.StdEncoding.EncodeToString(b))
 
 	pipeline.SetState(gst.StatePlaying)
 
@@ -53,6 +119,9 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("got sample", sample.Duration)
+		samples := uint32(90000 * (float32(sample.Duration) / 1000000000))
+		if err := videoTrack.WriteSample(media.Sample{Data: sample.Data, Samples: samples}); err != nil {
+			panic(err)
+		}
 	}
 }
