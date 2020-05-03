74d7cacd88ed1b8a2f398a933d1524890ca6afe0
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-05-04 02:20:22 +0900

    Split js file

---
+++ streaming.js
@@ -0,0 +1,46 @@
+const pc = new RTCPeerConnection({
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    }
+  ]
+});
+
+pc.ontrack = event => {
+  const video = document.querySelector("video");
+  if (video.srcObject == null) {
+    video.srcObject = event.streams[0];
+  } else {
+    video.srcObject.addTrack(event.track);
+  }
+};
+
+pc.oniceconnectionstatechange = () => console.log(pc.iceConnectionState);
+pc.onicecandidate = event => {
+  if (event.candidate == null) {
+    fetch("/connect", {
+      method: "POST",
+      body: JSON.stringify(pc.localDescription)
+    })
+      .then(res => Promise.all([res.json(), res.ok]))
+      .then(([answer, ok]) => {
+        if (!ok) {
+          return Promise.reject(answer);
+        }
+        try {
+          pc.setRemoteDescription(new RTCSessionDescription(answer));
+        } catch (e) {
+          Promise.reject(e);
+        }
+      })
+      .catch(console.error);
+  }
+};
+
+pc.addTransceiver("video", { direction: "recvonly" });
+pc.addTransceiver("audio", { direction: "recvonly" });
+pc.createDataChannel("heartbeat");
+
+pc.createOffer()
+  .then(d => pc.setLocalDescription(d))
+  .catch(console.error);
